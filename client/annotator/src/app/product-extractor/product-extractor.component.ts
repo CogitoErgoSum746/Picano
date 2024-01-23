a017e9ad60f565121194e1357797e53eca586fbc
vamsee <vamsee.somasi@gmail.com>
2024-01-23 18:54:54 +0530

    Reorder Product attributes

--- client/annotator/src/app/product-extractor/product-extractor.component.ts
+++ client/annotator/src/app/product-extractor/product-extractor.component.ts
@@ -17,20 +17,12 @@ export class ProductDetailsUpdator {
     // Pass the updated product details to parent component
     // to update products array.
     @Output() updateProduct: EventEmitter<Product> = new EventEmitter();
-    PRODUCT_LABELS: any = {
-        "name": "Product Name",
-        "brand": "Brand Name",
-        "description": "Product Description",
-        "discoutedPrice": "Discounted Product Price",
-        "campaignQuantity": "Campaign Quantity",
-        "restrictions": "Restrictions",
-    };
 
     // handleClick fetches the text for current cropped
     // image and assigns it to the relevant attribute of product.
     async handleClick(event: Event) {
         // Get product attribute.
-        const { name: productAttribute } = <HTMLButtonElement>event.target;
+        const { id: productAttribute } = <HTMLButtonElement>event.target;
 
         // Convert imageUrl to blob.
         if (!this.imageUrl) return;
