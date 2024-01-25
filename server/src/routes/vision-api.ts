0049cac61372e88df01e95d533a719e93fee488a
vamsee <vamsee.somasi@gmail.com>
2024-01-25 15:05:08 +0530

    Add PDF conversion API and integrate it with FE

--- server/src/routes/vision-api.ts
+++ server/src/routes/vision-api.ts
@@ -1,5 +1,7 @@
 import multer from 'multer';
 import { v1 as Vision } from '@google-cloud/vision';
+import pdf2img from 'pdf-img-convert';
+import fs from 'fs';
 
 import { Router, Request, Response, NextFunction } from 'express';
 const router: Router = Router();
@@ -27,6 +29,30 @@ router.post('/vision', upload.single('image'), async (req: Request, res: Respons
     } catch (error) {
         console.error('Error in vision api:', error);
         res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+router.post('/pdf-to-images', upload.single('pdf'), async (req: Request, res: Response) => {
+    try {
+        if (!req.file) return res.status(400).json({ "message": "No pdf provided."});
+
+        // Create folder if it doesn't exist.
+        fs.mkdirSync('./temp', { recursive: true });
+
+        // Store PDF locally. We need a path to provide for conversion.
+        fs.writeFileSync(`./temp/file.pdf`, req.file.buffer);
+
+        // Convert to images.
+        const images = await pdf2img.convert('./temp/file.pdf', { base64: true });
+
+        // Delete the PDF.
+        fs.unlinkSync('./temp/file.pdf');
+
+        res.json({"images": images});
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server error" });
     }
 });
 
