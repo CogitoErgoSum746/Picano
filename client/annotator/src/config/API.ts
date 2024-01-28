6539cdecb03aec33e54bd0769f8c43e0af0ed825
CogitoErgoSum746 <jamesgracious123@gmail.com>
2024-01-28 14:57:06 +0530

    fixed JWT auth

--- client/annotator/src/config/API.ts
+++ client/annotator/src/config/API.ts
@@ -1,5 +1,6 @@
 // IP or Domain of API endpoint.
 const baseURL = 'http://localhost:8000';
+// const baseURL = 'http://158.220.90.117/api';
 
 export const API = {
     dropdownFilter: (field='', id='', value='') => Boolean(field) ? `${baseURL}/auto-dropdown?${field}[id]=${id}&${field}[value]=${value}` : `${baseURL}/auto-dropdown`,
