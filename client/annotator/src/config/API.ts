7be40e8a94421e031f2fbffa6e577616f33a6714
vamsee <vamsee.somasi@gmail.com>
2024-01-28 21:05:56 +0530

    Submit data to API

--- client/annotator/src/config/API.ts
+++ client/annotator/src/config/API.ts
@@ -6,7 +6,7 @@ export const API = {
     dropdownFilter: (field='', id='', value='') => Boolean(field) ? `${baseURL}/auto-dropdown?${field}[id]=${id}&${field}[value]=${value}` : `${baseURL}/auto-dropdown`,
     login: `${baseURL}/auth/login`,
     vision: `${baseURL}/vision`,
-    profile: `${baseURL}/profile`,
+    submit: `${baseURL}/finalCSV`,
     pdfToImages: `${baseURL}/pdf-to-images`,
     productCategories: `${baseURL}/product_category`,
 }
