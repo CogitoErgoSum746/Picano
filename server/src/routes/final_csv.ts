c847b67e3e58e0383e35fc8d943ff533d6f31ca4
CogitoErgoSum746 <jamesgracious123@gmail.com>
2024-02-04 14:40:22 +0530

    csv ids updated

--- server/src/routes/final_csv.ts
+++ server/src/routes/final_csv.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { executeQuery } from '../db_connections/haukka_db';
+import { executeQuery, executeMultipleQueries } from '../db_connections/haukka_db';
 
 const router: Router = Router();
 
@@ -16,11 +16,46 @@ router.post('/finalCSV', async (req: Request, res: Response) => {
         const perRowPlaceholder = `(${Array.from({ length: 17 }, () => '?').join(', ')})` ;
         const placeholder = products.map(() => perRowPlaceholder).join(',');
 
+        async function getCampaignData(campaignData: any): Promise<number[]> {
+            const queries = [
+                {
+                    query: `SELECT group_id
+                            FROM admin_group
+                            WHERE group_name=? AND status = 'active';`,
+                    values: campaignData.group
+                },
+                {
+                    query: `SELECT chain_cat_id
+                            FROM admin_chain_category
+                            WHERE chain_cat_name=? AND status = 'active';`,
+                    values: campaignData.category
+                },
+                {
+                    query: `SELECT chain_id
+                            FROM admin_chain
+                            WHERE chain_name=? AND status = 'active';`,
+                    values: campaignData.chain
+                },
+                {
+                    query: `SELECT store_id
+                            FROM admin_store
+                            WHERE store_name_fi=? AND status = 'active';`,
+                    values: campaignData.store
+                },
+            ];
+
+            const [group_id, chain_cat_id, chain_id, store_id] = await executeMultipleQueries(queries);
+
+            return [group_id, chain_cat_id, chain_id, store_id];
+        }
+
+        const campaignIds = await getCampaignData(campaign);
+
         let values = products.map((product: any) => 
             ([ campaign.group, campaign.category, campaign.chain, campaign.store,
             product.brand, product.name, product.description, product.discountedPrice,
             product.campaignQuantity, product.restrictions, product.category,
-            product.from, product.to, groupId, categoryId, chainId, storeId ])
+            product.from, product.to, campaignIds[0], campaignIds[1], campaignIds[2], campaignIds[3]  ])
         );
 
         values = values.flat()
@@ -28,10 +63,10 @@ router.post('/finalCSV', async (req: Request, res: Response) => {
 
 
         const insertQuery = `
-INSERT INTO final_products (group_name, chain_category, chain_name, store_name_fi, brand_name, 
-                            product_name, product_description, discounted_product_price, 
-                            campaign_quantity, restrictions, product_category, campaign_start_date, campaign_end_date
-                            group_id, chain_category_id, chain_id, store_id) 
+INSERT INTO Final_products_csv (groupname, chainCategory, chain, storeNameFI, brandName, 
+                            productNameFI, productDescriptionFI, discountedProductPrice, 
+                            campaignQty, restrictions, productCategory1, campaignStartDate, campaignEndDate
+                            groupId, chainCategoryId, chainId, storeId) 
 VALUES ${placeholder};
 `;
         await executeQuery(insertQuery, values);
